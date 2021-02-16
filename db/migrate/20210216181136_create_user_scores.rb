6ff8126ba9a598fffd5d348e3868c26fd512f2a9
Alexander <acepetrov96@gmail.com>
2021-02-16 12:13:30 -0600

    Created and migrated database

--- db/migrate/20210216181136_create_user_scores.rb
+++ db/migrate/20210216181136_create_user_scores.rb
@@ -3,7 +3,7 @@ class CreateUserScores < ActiveRecord::Migration[6.1]
     create_table :user_scores do |t|
       t.integer :points
       t.belongs_to :User, null: false, foreign_key: true
-      t.belongs_to :Leaderboard, null: false, foreign_key: true
+      t.belongs_to :Leaderboard, foreign_key: true
 
       t.timestamps
     end
